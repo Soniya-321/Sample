8d38a501524cd091973bfc3c7281c7c5d64d3570
Soniya <boralakuntasoniya@gmail.com>
2025-06-17 11:33:42 +0530

    changed

---
+++ example.js
@@ -0,0 +1,4 @@
+const list1 = [1,2,3,4,5]
+const list2 = [6,7,8,9,10]
+const list3 = [...list1, ...list2];
+console.log("Combined list:", list3);
--- index.js
+++ index.js
@@ -2,4 +2,3 @@ const list = ["apple", "banana", "cherry"];
 
 console.log("Original list:", list);
 list.push("Strawberry");
-console.log("Updated list:", list);
