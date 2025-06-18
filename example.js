42646c02f6092deb692e4bb08d9099f272a6f0f3
Sai Sesha Reddy <saiseshar@gmail.com>
2025-06-18 12:11:11 +0530

    files changed

--- example.js
+++ example.js
@@ -1,4 +1,3 @@
-const list1 = [1,2,3,4,5]
-const list2 = [6,7,8,9,10]
-const list3 = [...list1, ...list2];
+const list1 = [1, 2, 3, 4, 5];
+const list2 = [6, 7, 8, 9, 10];
 console.log("Combined list:", list3);
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 const list = ["apple", "banana", "cherry"];
 
 console.log("Original list:", list);
-list.push("Strawberry");
